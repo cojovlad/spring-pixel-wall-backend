45660b5af13d20c7b2586d1f664ce241f6ed506d
Vlad-Andrei Cojocariu <vlad-andrei.cojocariu@ibm.com>
2025-11-24 23:02:34 +0200

    initial commit of backend

---
+++ src/main/java/com/pixelwall/SpringPixelWallBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pixelwall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPixelWallBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPixelWallBackendApplication.class, args);
+	}
+
+}
